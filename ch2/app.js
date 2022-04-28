67e4323e8adaac3ecd551373ba35a92e828cf667
Choinbong <101409368+Choinbong@users.noreply.github.com>
2022-04-29 00:23:53 +0900

    vanilla-js-220429
    return, conditional

--- ch2/app.js
+++ ch2/app.js
@@ -1,75 +1,111 @@
-const a = 5, b = 2;
-let myName = "JH";
+// const a = 5, b = 2;
+// let myName = "JH";
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
 
-myName = "Jehee";
+// myName = "Jehee";
 
-console.log("your new nmae is " + myName);
+// console.log("your new nmae is " + myName);
 
-const amIFat = true;    /* true, false, null */
-let something;
-console.log(something, amIFat);
+// const amIFat = true;    /* true, false, null */
+// let something;
+// console.log(something, amIFat);
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-const nonsense = [1, 2, "hello", false, null, true, undefined, "JH"];
+// const nonsense = [1, 2, "hello", false, null, true, undefined, "JH"];
 
-//Get Items from array
-console.log(daysOfWeek[5]);
+// //Get Items from array
+// console.log(daysOfWeek[5]);
 
-//Add one more day to the array
-daysOfWeek.push("sun")
+// //Add one more day to the array
+// daysOfWeek.push("sun")
 
-console.log(daysOfWeek);
+// console.log(daysOfWeek);
 
-/* 
-const player = {
-    name: "JH",
-    points: 10,
-    fat: true,
-}; 
-console.log(player);
-console.log(player.name);   //console.log(player[name]);
-player.lastName = "potato";
-console.log(player);
-*/
+// /*
+// const player = {
+//     name: "JH",
+//     points: 10,
+//     fat: true,
+// };
+// console.log(player);
+// console.log(player.name);   //console.log(player[name]);
+// player.lastName = "potato";
+// console.log(player);
+// */
 
-function sayHello(nameOfPerson, age) {
-    console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+// function sayHello(nameOfPerson, age) {
+//     console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+// }
+
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+// function plus(a, b) {
+//     console.log(a + b);
+// }
+// function divide(a, b) {
+//     console.log(a / b);
+// }
+// plus(8, 60);
+// divide(20, 5);
+
+// const player = {
+//     name: "nico",
+//     sayHello: function (otherPersonsName) {
+//         console.log("hello! " + otherPersonsName + " nice to meet you.");
+//     },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
+// player.sayHello("Jehee");
+
+// const calculator = {
+//     plus: function (a, b) {
+//         return a + b;
+//     },
+
+//     minus: function (a, b) {
+//         return a - b;
+//     },
+
+//     times: function (a, b) {
+//         return a * b;
+//     },
+
+//     divide: function (a, b) {
+//         return a / b;
+//     },
+
+//     power: function (a, b) {
+//         return a ** b;
+//     },
+// }
+
+// const plusResult = calculator.plus(2, 3);
+// const minusResult = calculator.minus(plusResult, 10);
+// const timesResult = calculator.times(10, minusResult);
+// const divideResult = calculator.divide(timesResult, plusResult);
+// const powerResult = calculator.power(divideResult, minusResult);
+
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0) {
+    console.log("Please write a real positive number.");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age === 100) {
+    console.log("Wow, you are wise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want.");
 }
-
-sayHello("nico", 10);
-sayHello("dal", 23);
-sayHello("lynn", 21);
-
-function plus(a, b) {
-    console.log(a + b);
-}
-function divide(a, b) {
-    console.log(a / b);
-}
-plus(8, 60);
-divide(20, 5);
-
-const player = {
-    name: "nico",
-    sayHello: function (otherPersonsName) {
-        console.log("hello! " + otherPersonsName + " nice to meet you.");
-    },
-};
-
-console.log(player.name);
-player.sayHello("lynn");
-player.sayHello("Jehee");
-
-const calculator = {
-    add: function (a, b) {
-        console.log(a + b);
-    },
-}
-
-calculator.add(5, 1);
